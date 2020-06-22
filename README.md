e772782f243d628481cb3b39698c85d203424b09
shaolang <shaolang@ymail.com>
2020-06-22 11:24:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# settler-kt: FX value date calculation library in Kotlin
+
+Copyright © 2020 Shaolang Ai
+
+Distributed under the Apache Software License 2.0
---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+/* Copyright 2020 Shaolang Ai
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
+
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    `java-library`
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    for (n in listOf("assertions-core", "core", "property", "runner-junit5")) {
+        testImplementation("io.kotest:kotest-$n-jvm:4.0.6")
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        showExceptions = true
+        showStandardStreams = true
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
